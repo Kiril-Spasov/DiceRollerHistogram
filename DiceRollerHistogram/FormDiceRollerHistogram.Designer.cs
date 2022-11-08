7a7adb1a47f5aa4d90ecba53ea56a80c014883ac
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-08 10:32:59 +0200

    Add project files.

---
+++ DiceRollerHistogram/FormDiceRollerHistogram.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DiceRollerHistogram
+{
+    partial class FormDiceRollerHistogram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnHorizontal = new System.Windows.Forms.Button();
+            this.BtnVertical = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // BtnHorizontal
+            // 
+            this.BtnHorizontal.Location = new System.Drawing.Point(64, 459);
+            this.BtnHorizontal.Name = "BtnHorizontal";
+            this.BtnHorizontal.Size = new System.Drawing.Size(151, 43);
+            this.BtnHorizontal.TabIndex = 0;
+            this.BtnHorizontal.Text = "Horizontal";
+            this.BtnHorizontal.UseVisualStyleBackColor = true;
+            this.BtnHorizontal.Click += new System.EventHandler(this.BtnHorizontal_Click);
+            // 
+            // BtnVertical
+            // 
+            this.BtnVertical.Location = new System.Drawing.Point(289, 459);
+            this.BtnVertical.Name = "BtnVertical";
+            this.BtnVertical.Size = new System.Drawing.Size(133, 42);
+            this.BtnVertical.TabIndex = 1;
+            this.BtnVertical.Text = "Vertical";
+            this.BtnVertical.UseVisualStyleBackColor = true;
+            this.BtnVertical.Click += new System.EventHandler(this.BtnVertical_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(494, 459);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(132, 42);
+            this.BtnExit.TabIndex = 2;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(35, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(738, 416);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // FormDiceRollerHistogram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 535);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnVertical);
+            this.Controls.Add(this.BtnHorizontal);
+            this.Name = "FormDiceRollerHistogram";
+            this.Text = "Dice Roller Histogram";
+            this.Load += new System.EventHandler(this.FormDiceRollerHistogram_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnHorizontal;
+        private System.Windows.Forms.Button BtnVertical;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ DiceRollerHistogram/FormDiceRollerHistogram.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DiceRollerHistogram
+{
+    public partial class FormDiceRollerHistogram : Form
+    {
+        public FormDiceRollerHistogram()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+
+        private void BtnHorizontal_Click(object sender, EventArgs e)
+        {        
+            int[] frequency = new int[13];
+            DiceRollSimulation(frequency);
+            DisplayHorizontalHistogram(frequency);
+        }
+
+        private void DiceRollSimulation(int[] f)
+        {
+            ClearArray(f);
+            Random r = new Random();
+            int dice1, dice2, sum;
+
+            //We start from 2, because there is no sum of 2 dice = 1.
+            for (int i = 2; i <= 1000; i++)
+            {
+                dice1 = r.Next(1, 7);
+                dice2 = r.Next(1, 7);
+                sum = dice1 + dice2;
+                f[sum] += 1;
+            }
+        }
+
+        //We take the largest number from our data and divide it by the size of the screen.
+        //We use the result to calculate later how many pixels for each chart.
+        private double FindScalingFactor(int[] f)
+        {
+            int biggest = f[1];
+            double scalingFactor = 0;
+
+            for (int i = 2; i <= f.Count() - 1; i++)
+            {
+                if (f[i] > biggest)
+                    biggest = f[i];
+            }
+
+            scalingFactor = Convert.ToDouble(biggest) / (panel1.Width - 200);
+
+            return scalingFactor;
+        }
+
+        private void DisplayHorizontalHistogram(int[] f)
+        {
+            g.Clear(panel1.BackColor);
+
+            //We use Random for random colors.
+            Random r = new Random();
+
+            double sf = FindScalingFactor(f);
+
+            Font font = new Font("Verdana", 8);
+            SolidBrush brush = new SolidBrush(Color.Blue);
+            
+
+            g.DrawString("Dice Roller Histogram", font, brush, panel1.Width / 2, 10);
+            g.DrawString("Roll", font, brush, 1, 50);
+            g.DrawString("Frequency", font, brush, 40, 50);
+
+            for (int i = 2; i <= 12; i++)
+            {
+                g.DrawString(i.ToString(), font, brush, 1, 50 + i * 20);
+                g.DrawString(f[i].ToString(), font, brush, 40, 50 + i * 20);
+
+                Color randomColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                SolidBrush brushRandomColor = new SolidBrush(randomColor);
+
+                //We use the scaling factor to calculate how many pixels for the width of each rectangle.
+                int rectangleWidth = (int)(f[i] / sf);
+
+                g.FillRectangle(brushRandomColor, 100, 50 + i * 20, rectangleWidth, 15);
+            }
+        }
+
+        private void BtnVertical_Click(object sender, EventArgs e)
+        {
+            int[] frequency = new int[13];
+            DiceRollSimulation(frequency);
+            DisplayVerticalHistogram(frequency);
+        }
+
+        private double FindVerticalScalingFactor(int[] f)
+        {
+            int biggest = f[1];
+            double vercticalSf;
+
+            for (int i = 1; i <= f.Count() - 1; i++)
+            {
+                if (f[i] > biggest)
+                    biggest = f[i];
+            }
+
+            vercticalSf = Convert.ToDouble(biggest) / (panel1.Height - 125);
+
+            return vercticalSf;
+        }
+
+        private void DisplayVerticalHistogram(int[] f)
+        {
+            g.Clear(panel1.BackColor);
+
+            Random r = new Random();
+
+            double verticalSf = FindVerticalScalingFactor(f);
+
+            Font font = new Font("Verdana", 8);
+            SolidBrush brush = new SolidBrush(Color.Blue);
+            g.DrawString("Dice Roller Histogram", font, brush, panel1.Width / 2, 10);
+
+            for (int i = 2; i <= f.Count() - 1; i++)
+            {
+                g.DrawString(i.ToString(), font, brush, 50 + i * 35, panel1.Height - 50);
+
+                //We use the scaling factor to calculate how many pixels is the height for each rectangle.
+                int rectangleHeight = (int)(f[i] / verticalSf);
+
+                Color randomColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                SolidBrush brushRandomColor = new SolidBrush(randomColor);
+                g.FillRectangle(brushRandomColor, 50 + i * 35, panel1.Height - 50 - rectangleHeight, 15, rectangleHeight);
+
+                g.DrawString(f[i].ToString(), font, brush, 45 + i * 35, panel1.Height - 75 - rectangleHeight);
+            }
+        }
+
+        private int[] ClearArray(int[] ar)
+        {
+            for (int i = 0; i <= ar.Count() - 1; i++)
+            {
+                ar[i] = 0;
+            }
+
+            return ar;
+        }
+
+        private void FormDiceRollerHistogram_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
